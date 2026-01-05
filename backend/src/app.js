c25a778d28e44511382cdbd5b11112d25889be41
debayansaha <grindart004@gmail.com>
2026-01-05 20:16:12 +0530

    production

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,14 @@ const cors = require("cors")
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://revi-8xun.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json())
 
 app.get('/',(req, res)=>{
