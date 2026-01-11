c072cf5263857f800ab9ba8994dfb97ab75fedea
debayansaha <grindart004@gmail.com>
2026-01-11 22:46:19 +0530

    R1

--- backend/src/email/sendOtp.js
+++ backend/src/email/sendOtp.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 
 module.exports.sendOtp = async(email , otp)=>{
     const transporter = nodemailer.createTransport({
-        service:"gmail",
+        host:process.env.BREVO_SMTP_HOST,
+        port:process.env.BREVO_SMTP_PORT,
+        secure: false, // port 587 uses STARTTLS
         auth: {
-            user: process.env.MAIL_USER, //tells gmail who is sending mail
-            pass: process.env.MAIL_PASSWORD, //proves that this server is allowed to send gmail through the above accnt, without this mail refuses to connect
+            user: process.env.BREVO_USER, //tells gmail who is sending mail
+            pass: process.env.BREVO_PASS, //proves that this server is allowed to send gmail through the above accnt, without this mail refuses to connect
         },
     })
-
-    //This is the actual message, tells from where mail is been send, who to send to , subject and content
+    
     const emailConfiguration ={
-        from: process.env.MAIL_USER,
+        from: process.env.MAIL_FROM,
         to:email,
         subject: "Reset Password",
         html: `<p> Your OTP for password reset is : <br/> <b>${otp}</b> <br/> It is valid for 5mins. </p>`
@@ -20,8 +21,10 @@ module.exports.sendOtp = async(email , otp)=>{
 
     //delivers the email
     transporter.sendMail(emailConfiguration, function(error,info){
-        if(error) throw new Error(error);
-        console.log('Email sent succesully !');
-        console.log(info);
+       if (error) {
+            console.error("MAIL ERROR ->", error);
+            return;
+        }
+        console.log("MAIL INFO ->", info);
     })
 }
